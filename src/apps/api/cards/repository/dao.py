71141cc699d930c45ba6df8a119c1bfebb8fb2ed
Pedro <87550035+odiseo0@users.noreply.github.com>
2026-07-13 13:31:00 -0400

    remove boiler plate code

--- src/apps/api/cards/repository/dao.py
+++
@@ -1,8 +0,0 @@
-from src.apps.api.cards.domain.entities import Card
-
-
-class InMemoryCardRepository:
-    async def search(self, query: str) -> list[Card]:
-        if not query:
-            return []
-        return [Card(id="demo-card", name=query, quantity=1)]
