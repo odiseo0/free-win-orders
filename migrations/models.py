525dd283ed63ba1aa32bc04d5a9be23b1ea39dcc
Pedro <87550035+odiseo0@users.noreply.github.com>
2026-07-22 16:51:51 -0400

    migrations for ordersrequest

--- migrations/models.py
+++ migrations/models.py
@@ -3,5 +3,10 @@ from src.api.order_periods.repository.models import (  # noqa: F401
     OrderPeriod,
     OrderPeriodHistory,
 )
+from src.api.order_requests.repository.models import (  # noqa: F401
+    OrderRequest,
+    OrderRequestHistory,
+    OrderRequestItem,
+)
 from src.api.roles.repository.models import Permission, Role, RolePermission  # noqa: F401
 from src.api.users.repository.models import User, UserAddress, UserRole  # noqa: F401
